9cb168d64ebe940e4a12723a8ca62ff180233651
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-05 21:45:22 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
-function main {
+function main () {
     const last = document.createElement('p')
     const loader = document.querySelector('.lastPost')
     loader.innerText = 'Telechargement....'
     Wrapper.append(loader) 
 
-    await fetch('Lien de lAPI') {
+    await fetch('Lien de lAPI', {
         Headers: {
             Accept: 'Application/json'
         }
-    }
+    })
 }
