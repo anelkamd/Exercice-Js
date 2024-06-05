0e46e4f8561f5f5acb91be1de9a3f746457389d8
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-05 21:48:44 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
 
-function main () {
+ async function main () {
     const last = document.createElement('p')
     const loader = document.querySelector('.lastPost')
     loader.innerText = 'Telechargement....'
     Wrapper.append(loader) 
 
-    await fetch('Lien de lAPI', {
+     const recq = await fetch('Lien de lAPI', {
         Headers: {
             Accept: 'Application/json'
         }
     })
+    if(!recq.ok) {
+        loader.innerText = 'Impossible de se Connecter'
+    }
 }
